c17ef7b21f38b038ffd2c54f8f27c03645f99f4d
Andres Dev <gmila@uphomesolution.com>
2025-05-06 21:13:09 -0500

    agregar camkpos, logica de rangos y de cantidad d placas y baterias

---
+++ src/app/data/ranks.ts
@@ -0,0 +1,18 @@
+export interface Rango {
+    id: number;
+    nombre: string;
+    porcentaje: number;
+  }
+  
+  export const rangos: Rango[] = [
+    {id : 1, nombre: "Trainne", porcentaje: 5},
+    {id : 2, nombre: "Consultant", porcentaje: 7},
+    {id : 3, nombre: "Leader", porcentaje: 9},
+    {id : 4, nombre: "Senior Leader", porcentaje: 9.5},
+    {id : 5, nombre: "Supervisor", porcentaje: 10},
+    {id : 6, nombre: "Senior supervisor", porcentaje: 10.5},
+    {id : 7, nombre: "Manager", porcentaje: 11},
+    {id : 8, nombre: "Senior Manager", porcentaje: 11.5},
+    {id : 9, nombre: "Vice president", porcentaje: 12}
+  ];
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,230 @@
-import Image from "next/image";
+'use client'
+
+import { useState, useEffect } from "react";
+import { rangos } from "@/app/data/ranks";
+import Image from 'next/image'
+import logoColor from "@/app/img/logoC.png";
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [idVendedor, setIdVendedor] = useState<string>("")
+  const [paneles, setPaneles] = useState<number>(10)
+  const [bateria, setBateria] = useState<string>("")
+  const [cantBateria, setCantBateria] = useState<number>(0)
+  const [catnBatBloq, setCatnBatBloq] = useState<boolean>(false)
+  const [listOpciones, setListOpciones] = useState<any[]>(["1"])
+  const [adder1, setAdder1] = useState<boolean>(false)
+  const [adder2, setAdder2] = useState<boolean>(false)
+  const [valorCampo, setValorCampo] = useState<string>("")
+
+  useEffect(() => {
+
+
+    // Effect número de baterias
+    if (bateria == "Tesla") {
+
+
+      if (paneles <= 24) {
+        // Obligatorio 1
+        setCantBateria(1)
+        setCatnBatBloq(true)
+        setListOpciones(["1"])
+      }
+
+      if (paneles >= 25 && paneles <= 34) {
+        // Opcional 2
+        setCantBateria(2)
+        setCatnBatBloq(false)
+        setListOpciones(["1", "2"])
+      }
+
+
+      if (paneles >= 35 && paneles <= 49) {
+        // Obligatorio 2
+        setCantBateria(2)
+        setCatnBatBloq(true)
+        setListOpciones(["2"])
+      }
+
+      if (paneles >= 50 && paneles <= 61) {
+        // Opcional 3
+        setCantBateria(3)
+        setCatnBatBloq(false)
+        setListOpciones(["2", "3"])
+      }
+
+      if (paneles >= 62 && paneles <= 73) {
+        // Obligatorio 3
+        setCantBateria(3)
+        setCatnBatBloq(true)
+        setListOpciones(["3"])
+      }
+
+      if (paneles >= 74) {
+        // Opcional 4
+        setCantBateria(4)
+        setCatnBatBloq(false)
+        setListOpciones(["3", "4"])
+      }
+    }
+
+
+
+    if (bateria == "Solar_Edge") {
+      
+      if (paneles <= 25) {
+        // Obligatorio 1
+        setCantBateria(1)
+        setCatnBatBloq(true)
+        setListOpciones(["1"])
+      }
+
+      if (paneles >= 26 && paneles <= 37) {
+        // Opcional 2
+        setCantBateria(2)
+        setCatnBatBloq(false)
+        setListOpciones(["1", "2"])
+      }
+
+      if (paneles >= 38) {
+        // Obligatorio 2
+        setCantBateria(2)
+        setCatnBatBloq(true)
+        setListOpciones(["2"])
+      }
+
+    }
+
+
+
+  }, [paneles, bateria]);
+
+
+
+  const handleCheck1 = (e:any) => {
+    setAdder1(e.target.checked)
+  };
+  const handleCheck2 = (e:any) => {
+    setAdder2(e.target.checked)
+  };
+
+
+  const handleClick = ()=>{
+    
+  }
+
+  return (
+    
+    <div className="container">
+      <div className="logo">
+        <Image
+          src={logoColor}
+          width={200}
+          height={100}
+          alt="Logo Up Home Solution."
+        />
+      </div>
+
+      <div className="bloqueForm">
+        <form onSubmit={(e) => e.preventDefault()}>
+          <label>
+            <span>Rango de Vendedor</span>
+
+            <select name="tipoVendedor" onChange={(e:any) => {setIdVendedor(e.target.value)}}>
+              <option value=""></option>
+              {rangos.map((item) => (
+                <option key={item.id} value={item.id}>{item.nombre}</option>
+              ))}
+            </select>
+          </label>
+
+          <label>
+            <span>Cantidad de Paneles</span>
+
+              <input
+                type="number"
+                name="paneles"
+                min="10"
+                max="75"
+                onChange={(e:any) => {setPaneles(e.target.value)}}
+                value={paneles}
+              />
+          </label>
+
+          <label>
+            <span>Tipo de Batería</span>
+
+              <select name="tipoBateria" onChange={(e:any) => {setBateria(e.target.value)}}>
+                <option value=""></option>
+                <option value="Solar_Edge">S. Edge</option>
+                <option value="Tesla">Tesla</option>
+              </select>
+
+          </label>
+              
+          <label>
+            <span>Número de Baterías</span>
+            <select
+              name="numeroBaterias"
+              onChange={(e:any) => {setCantBateria(e.target.value)}}
+              disabled={catnBatBloq}
+              value={cantBateria}
+            >
+              
+              {listOpciones.map(item=>(
+                <option value={item} key={item}>{item}</option>
+              ))}
+
+            </select>
+          </label>
+
+          <div className="selectores">
+            <label>
+              <span>Adder 1</span>
+              <input
+                type="checkbox"
+                name="adder1"
+                onChange={handleCheck1}
+                checked={adder1}
+              />
+            </label> 
+            <label>
+              <span>Adder 2</span>
+              <input
+                type="checkbox"
+                name="adder2"
+                onChange={handleCheck2}
+                checked={adder2}
+              />
+            </label>
+          </div>
+
+        </form>
+
+        <div className="visualizador">
+              <div className="epcBase"></div>
+              <div className="totalBase"></div>
+
+              <label>
+                <span>EPC Vemdodo</span>
+                <input type="number"  onChange={(e:any) => {setValorCampo(e.target.value)}} value={valorCampo}/>
+              </label>
+
+              <button type="button" onClick={handleClick}>Hola mundo</button>
+
+              <div className="comision">
+                
+              </div>
+              <div className="epctotal"></div>
+
+              <button type="button" onClick={handleClick}>Modificar valores</button>
+              <button type="button" onClick={handleClick}>Nueva cotización</button>
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+      </div>
+      </div>
+
   );
 }
